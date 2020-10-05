17633579f4aa328e42deafb93f73027a954b0503
ryjm <jraydermiller@gmail.com>
2020-10-05 17:54:24 -0400

    wrap entire review-preview in a link

--- src/js/components/lib/review-preview.js
+++ src/js/components/lib/review-preview.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
@@ -20,27 +19,25 @@ export class ReviewPreview extends Component {
     const itemLink = stackLink + '/' + this.props.item.itemName;
     const loc = {
       pathname: itemLink,
-      state: { mode: 'review',
-              prevPath: location.pathname }
+      state: { mode: 'review', prevPath: location.pathname },
     };
     return (
-      <div className="f9 lh-solid ml3 flex">
-        <div className="flex-wrap">
-          <div className="mv2 link black dim db mw5 pa2 br2 bt b--green0 shadow-hover ma2">
-            <Link to={loc}>
+      <Link to={loc}>
+        <div className="f9 lh-solid ml3 flex">
+          <div className="flex-wrap">
+            <div className="mv2 link black dim db mw5 pa2 br2 bt b--green0 shadow-hover ma2">
               <ItemSnippet body={this.props.item.itemSnippet} />
-            </Link>
-            <div className="flex">
-              <div className="gray2 mr3">{author}</div>
-              <div className="gray2 mr2">{date}</div>
-             <Link to={stackLink} >
-              <div className="blue3 mr2">{this.props.item.stackTitle}</div>
-             </Link>
-
+              <div className="flex">
+                <div className="gray2 mr3">{author}</div>
+                <div className="gray2 mr2">{date}</div>
+                <Link to={stackLink}>
+                  <div className="blue3 mr2">{this.props.item.stackTitle}</div>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
